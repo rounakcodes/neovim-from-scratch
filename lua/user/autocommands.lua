7c1e6aeb55e5b17acf80c2dd19eec7115c0d30d0
Rounak Jain <rounakjainofficial@gmail.com>
2022-06-17 12:14:42 +0530

    auto save

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -28,6 +28,11 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+  
+  augroup AUTOSAVE
+    au!
+    autocmd InsertLeave, TextChanged, FocusLost * silent! update
+  augroup END
 ]]
 
 -- Autoformat
